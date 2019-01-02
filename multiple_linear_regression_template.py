21f87cd820dcd24530ac608b026e16a71c3a086f
Kaveen Hyacinth <alwiskaveen@gmail.com>
2019-01-02 07:08:12 +0530

    Remove manual backward elimination

--- multiple_linear_regression_template.py
+++ multiple_linear_regression_template.py
@@ -41,24 +41,6 @@ y_pred = regressor.predict(X_test)
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50 ,1)).astype(int),values = X, axis = 1)
 
-'''
-X_opt = X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[:, [0, 1, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[:, [0, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[:, [0, 3, 5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[:, [0, 3]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-'''
-
 # automatic backward elimination only p-value
 
 def backwardElimination(x, sl):
